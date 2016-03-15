fdf2ab35c7b7ea27670c4139ed60048869770ff5
proietfb <fabio.proietti.91@gmail.com>
2016-03-15 12:12:02 +0000

    Inglobate le classi RMIServer e Client nella classe DiceLiar

--- ExampleOK/src/DiceLiar.java
+++ ExampleOK/src/DiceLiar.java
@@ -11,6 +11,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
@@ -30,6 +31,8 @@ public class DiceLiar extends UnicastRemoteObject implements RMI{
         currentBoard = _currentBoard;
     }
 
+    private DiceLiar() throws RemoteException{}
+
     @Override
     public String getData(String text) throws RemoteException {
         return Integer.toString(currentBoard.getnPlayers());
@@ -41,6 +44,36 @@ public class DiceLiar extends UnicastRemoteObject implements RMI{
     public Board getBoard() throws RemoteException {
         return currentBoard; 
     }
+    
+    private void connectServer(){
+      //  if (System.getSecurityManager() == null) {
+            //System.setSecurityManager(new SecurityManager());
+       // }
+        try{
+            
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.println("Inserisci il tuo indirizzo:");
+            
+            String addr = scanner.next();
+            
+
+            Registry reg= LocateRegistry.getRegistry(addr, 40000);
+            //ottengo lo stub dell'oggetto remoto contenuto nel server
+            RMI rmi = (RMI) reg.lookup("server");
+            System.out.println("Connected to server");
+
+            //uso il metodo remoto
+            Board text = rmi.getBoard();
+            System.out.println(Integer.toString(text.getnPlayers()));
+
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
     
     /**/
     
@@ -56,11 +89,30 @@ public class DiceLiar extends UnicastRemoteObject implements RMI{
         DiceLiar obj = new DiceLiar(startBoard);
             //creo un registro e vi collego il metodo associandolo come istanza
             //ad un nome ("server")
+            
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Definisci il tuo status:(Server o Client)");
+        
+        String status = scanner.next();
+        
+        if (status == "Server" || status == "server" || status == "S" || status == "s"){
+                
+                Registry reg = LocateRegistry.createRegistry(40000);
+                reg.bind("server", obj);
 
-        Registry reg = LocateRegistry.createRegistry(40000);
-        reg.bind("server", obj);
+                System.out.println("Server Started...");
+        }
+        
+        else if (status == "Client" || status == "client" || status == "C" || status == "c"){
+            
+            DiceLiar client = new DiceLiar();
+            client.connectServer();
+            
+        }
 
-        System.out.println("Server Started...");
+
 
         //startBoard.initGame(startBoard);
 
