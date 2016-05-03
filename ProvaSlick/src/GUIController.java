0047fb4aadf4861722bc1b3e15e64f6429b7031b
gorgo <gorgo@Dago.(none)>
2016-05-03 14:19:48 +0200

    Add GUIController

---
+++ ProvaSlick/src/GUIController.java
@@ -0,0 +1,132 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gorgo
+ */
+public class GUIController {
+    boolean initBoardBool; //Primo Update della Board Play
+    
+    int turn; // Numero turn
+    int id; //ID current Player
+    
+    int initDicePlayer; //Numero iniziale di dadi del giocatore
+    int nPlayers; // Numero giocatori attivi
+       
+    
+    // Check button Clicked
+    boolean PlayConnectedClicked = false;
+    boolean makeBetClicked = false;
+    boolean doubtClicked = false;
+    
+    boolean betClicked = false;
+    boolean leaveClicked = false;
+    
+    int diceValueSelected;
+    int diceAmountSelected;
+
+    public GUIController() {
+    }
+
+    public boolean isPlayConnectedClicked() {
+        return PlayConnectedClicked;
+    }
+
+    public void setPlayConnectedClicked(boolean PlayConnectedClicked) {
+        this.PlayConnectedClicked = PlayConnectedClicked;
+    }
+
+    public boolean isBetClicked() {
+        return betClicked;
+    }
+
+    public void setBetClicked(boolean betClicked) {
+        this.betClicked = betClicked;
+    }
+
+    public int getDiceAmountSelected() {
+        return diceAmountSelected;
+    }
+
+    public void setDiceAmountSelected(int diceAmountSelected) {
+        this.diceAmountSelected = diceAmountSelected;
+    }
+
+    public int getDiceValueSelected() {
+        return diceValueSelected;
+    }
+
+    public void setDiceValueSelected(int diceValueSelected) {
+        this.diceValueSelected = diceValueSelected;
+    }
+
+    public boolean isDoubtClicked() {
+        return doubtClicked;
+    }
+
+    public void setDoubtClicked(boolean doubtClicked) {
+        this.doubtClicked = doubtClicked;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isInitBoardBool() {
+        return initBoardBool;
+    }
+
+    public void setInitBoardBool(boolean initBoardBool) {
+        this.initBoardBool = initBoardBool;
+    }
+
+    public int getInitDicePlayer() {
+        return initDicePlayer;
+    }
+
+    public void setInitDicePlayer(int initDicePlayer) {
+        this.initDicePlayer = initDicePlayer;
+    }
+
+    public boolean isLeaveClicked() {
+        return leaveClicked;
+    }
+
+    public void setLeaveClicked(boolean leaveClicked) {
+        this.leaveClicked = leaveClicked;
+    }
+
+    public boolean isMakeBetClicked() {
+        return makeBetClicked;
+    }
+
+    public void setMakeBetClicked(boolean makeBetClicked) {
+        this.makeBetClicked = makeBetClicked;
+    }
+
+    public int getnPlayers() {
+        return nPlayers;
+    }
+
+    public void setnPlayers(int nPlayers) {
+        this.nPlayers = nPlayers;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
+    
+    
+    
+}
---
+++ ProvaSlick/src/RMIGameController.java
@@ -0,0 +1,201 @@
+
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gorgo
+ */
+public class RMIGameController extends UnicastRemoteObject implements RMI {
+    
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    
+    Board rmiBoard;
+    public final Object lock;
+    int myID;
+    
+    public RMIGameController() throws RemoteException {
+     lock = new Object();
+     myID = -1;
+    }
+
+    @Override
+    public ArrayList<PlayerEntry> addClient(String ipPlayer, int portPlayer) throws RemoteException {
+        return null;
+    }
+    
+    @Override
+    public int getTimer() throws RemoteException {
+        return -1;
+    }
+
+    @Override
+    public int[] getDice(int idPlayer, Players currentPlayers) throws RemoteException {
+        return rmiBoard.currentPlayers.vectorPlayers[idPlayer].getmyDiceValue();
+    }
+
+    @Override
+    public boolean setDice(int id, Players playersArray) throws RemoteException {
+        boolean lastChange = true;
+        
+        //System.out.println("SET DICE e DiceUpdated = " + rmiBoard.diceUpdated);
+        
+        if(rmiBoard.diceUpdated == rmiBoard.currentPlayers.getVectorPlayers().length){
+            System.out.println("DADI COMPLETAMENTE AGGIORNATI");
+            return lastChange;  
+        }
+            
+        
+        //System.out.println("Sono " + myID + " e mi ha sbloccato " + id);
+        for (int i=0; i<playersArray.getVectorPlayers().length;i++) {
+            
+            //if(rmiBoard.currentPlayers.vectorPlayers[i].getMyDiceObject() == null)
+            //System.out.println(i + ": RMIBoard getMyDiceObject NULL");
+            //if(playersArray.getVectorPlayers()[i].getMyDiceObject() != null)
+                //System.out.println(i + ": playersArray getMyDiceObject NOT NULL");
+            
+            if(rmiBoard.currentPlayers.vectorPlayers[i].getMyDiceObject() == null && playersArray.getVectorPlayers()[i].getMyDiceObject() != null){
+                System.out.println(myID + ": aggiorno i miei dadi con quelli del giocatore " + i);         
+                rmiBoard.diceUpdated++;
+                rmiBoard.currentPlayers.vectorPlayers[i] = playersArray.getVectorPlayers()[i];
+                
+                lastChange = false;
+
+                if(rmiBoard.diceUpdated == rmiBoard.currentPlayers.getVectorPlayers().length){
+                    //System.out.println("Ho aggiornato rmiBoard diceUpdate " + rmiBoard.diceUpdated + " giocatori");
+                    
+                    System.out.println("\n------------------TUTTI I DADI---------------------------");
+                    rmiBoard.currentPlayers.printDice();
+                    System.out.println("\n");
+//                    for (int j = 0; j < rmiBoard.currentPlayers.getAllId().length; j++) {
+//                        int[] myDice = rmiBoard.currentPlayers.getVectorPlayers()[j].getmyDiceValue();
+//                        for (int z = 0; z < myDice.length; z++) {
+//                            System.out.println("Player " + j + ": " + myDice[z]);
+//                        }
+//                    }
+                    
+                    lastChange = true;
+                    rmiBoard.status = Board.PLAYING;
+                }
+                    
+            }  
+        }
+        //rmiBoard.currentPlayers.vectorPlayers[id].setMyDice(dice);
+        synchronized (lock) {
+            rmiBoard.ready = true;
+            lock.notify();
+            //System.out.println(myID + " NOTIFY");
+        }
+        return lastChange;
+    }
+
+    @Override
+    public void notifyTurn(Board board) throws RemoteException{   
+        rmiBoard.setnTurn(board.getnTurn());
+        rmiBoard.setCurrentBet(board.getCurrentBet());
+        
+        //if(rmiBoard.getCurrentBet() == null || board.getCurrentBet() == null)
+           //rmiBoard.getCurrentPlayers().vectorPlayers[myID].makeChoice(rmiBoard);
+        
+        rmiBoard.setPlayingPlayer(board.getPlayingPlayer());
+        rmiBoard.getPlayingPlayer().setTurn(true);        
+        rmiBoard.getCurrentPlayers().getVectorPlayers()[myID].setTurn(true);
+        rmiBoard.setPlayingPlayer(board.getPlayingPlayer());
+        
+       // System.out.println("Sblocco il giocatore " + rmiBoard.myID);
+        
+        rmiBoard.status = board.status;
+        
+        synchronized (rmiBoard.lock){
+            rmiBoard.ready = true;
+            rmiBoard.lock.notify();
+            if(rmiBoard.status == Board.PLAYING){
+                //System.out.println("NotifyTurn: Il turno CONTINUA\n");
+            }
+            //else if(rmiBoard.status == Board.RESET){
+               // System.out.println("NotifyTurn: Io " + rmiBoard.myID + " Devo resettare i dadi\n");
+            //}
+        }
+    }
+
+    @Override
+    public void resetDice(Players currentPlayers) throws RemoteException {
+        //System.out.println("-------------------------------------------------------------------------------------------");
+        //System.out.println("ID: " + myID + " RESET DICEUpDated");
+        rmiBoard.getCurrentPlayers().resetAllDice(myID);
+        
+        rmiBoard.diceUpdated = 1;  
+        rmiBoard.status = Board.RESET;
+        rmiBoard.setnTurn(1);
+
+        
+        System.out.println("------------------DADI LOCALI CREATI NELLA RESETDICE---------------------------");
+        int[] myDice = rmiBoard.getCurrentPlayers().getVectorPlayers()[myID].getmyDiceValue();
+        for(int i=0;i<myDice.length;i++)
+            System.out.println("Player " + myID + ": " + myDice[i]);
+        //shareDice(currentPlayers, rmiNext);
+        
+        synchronized (rmiBoard.lock){
+            rmiBoard.ready = true;
+            rmiBoard.lock.notify();
+        }
+    }
+
+    @Override
+    public void checkDoubtRMI(Board board) throws RemoteException {
+       System.out.println(ANSI_CYAN + "Il giocatore " + board.getPlayingPlayer().myID + " ha detto che sul tavolo NON ci sono almeno " + board.getCurrentBet().amountDice + " dadi di valore " + board.getCurrentBet().valueDie + ANSI_RESET);
+       int[] vectorDice = board.getCurrentPlayers().getAllDice(true); // Stampo tutti i dadi
+       if(board.okDoubt){
+           System.out.println(ANSI_GREEN + "Ha ragione, ce ne sono " + vectorDice[board.getCurrentBet().valueDie-1] + "! Tocca a lui iniziare il nuovo giro" + ANSI_RESET);
+           if( (((board.getPlayingPlayer().myID - myID)%board.getnPlayers()) + board.getnPlayers())%board.getnPlayers() == 1){
+               System.out.println(ANSI_RED + "Perdo un dado :(" + ANSI_RESET);
+               rmiBoard.getCurrentPlayers().getVectorPlayers()[myID].getMyDiceObject().removeDie();
+           }        
+       }
+       else
+           System.out.println(ANSI_RED + "Non ha ragione, ce ne sono " + vectorDice[board.getCurrentBet().valueDie-1] + "! Perde un dado e inizia il giocatore successivo" + ANSI_RESET);
+       
+       System.out.println("\n-------------------------------------- NUOVO TURNO -------------------------------------------\n");
+
+    }
+
+    @Override
+    public void setRestart() throws RemoteException {
+        //System.out.println("QUELLO PRIMA DI ME HA ERRONEAMENTE DUBITATO");
+        rmiBoard.getCurrentPlayers().getVectorPlayers()[myID].setTurn(true);
+        rmiBoard.setPlayingPlayer(rmiBoard.getCurrentPlayers().getVectorPlayers()[myID]);
+        rmiBoard.diceUpdated = 1;
+        rmiBoard.status = Board.INIT_RESET;
+        
+        rmiBoard.setCurrentBet(null);
+        
+        synchronized (rmiBoard.lock){
+            rmiBoard.ready = true;
+            rmiBoard.lock.notify();
+        }
+    }
+
+    @Override
+    public void oneIsOne(Board board) throws RemoteException {
+        System.out.println("Uno vale uno. Non posso più usarlo come jolly fino al prossimo turno");
+        rmiBoard.oneJollyEnabled = false;
+        rmiBoard.currentPlayers.printDice();
+    }  
+
+    @Override
+    public void notifyMove(Board board) throws RemoteException {
+        System.out.println("Il giocatore " + board.getPlayingPlayer().myID + " ha rilanciato con " + board.getCurrentBet().amountDice + " dadi di valore " + board.getCurrentBet().valueDie);
+    }
+    
+}
